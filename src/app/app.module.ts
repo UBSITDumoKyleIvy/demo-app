243ad6c63fbe8edd3dc632832b18de516c62c56b
UBSITDumoKyleIvy <20215655@s.ubaguio.edu>
2023-01-30 10:14:42 +0800

    app.component.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NewComponentComponent } from './components/new-component/new-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    NewComponentComponent,
-    
+  
   ],
   imports: [
     BrowserModule,
